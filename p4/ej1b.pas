755b3ac2918c383779760d695e82732ce3495adf
Santiago <santiagoberriel23@gmail.com>
2026-08-23 19:07:15 -0300

    ej1b

--- p4/ej1b.pas
+++ p4/ej1b.pas
@@ -4,11 +4,11 @@ program Ejercicio1b;
         for b := 1 to 3 do
     begin
     if b <= 1 then
-        a := b - 1;
+        a := b - 1; // a = 0
     if b <= 2 then
-        a := a - 1
+        a := a - 1 // a = -1 || a = -2
     else
-        a := a + 1
+        a := a + 1 // a = -1
     end;
         writeln(a)
 end.
