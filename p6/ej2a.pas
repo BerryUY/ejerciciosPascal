4153af437e3c83a9d5cc90cd42b078f3cf5d12f3
Santiago <santiagoberriel23@gmail.com>
2026-08-26 16:30:12 -0300

    update

--- p6/ej2a.pas
+++ p6/ej2a.pas
@@ -13,5 +13,27 @@ begin
 
     cant := 0;
 
-    
+    { avanzar hasta comienzo de primera palabra }
+    repeat
+        read(car);
+    until car <> ESPACIO;
+
+    repeat
+        { avanzar hasta terminar la palabra }
+        repeat
+            car_ant := car;
+            read(car);
+        until (car = ESPACIO) OR (car = FIN);
+
+        { control de fin de palabra }
+        if (car_ant = letra) then
+            cant := cant + 1;
+
+        { avanzar hasta comienzo siguiente palabra }
+        while car = ESPACIO do
+            read(car);
+
+until (car=FIN);
+
+writeln('La oracion tiene ', cant, ' palabras que terminan con ', letra);
 end.
